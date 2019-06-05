91daf7e8695a03e5e56a24b946f80f8ddbf1acdb
PengChen <fengyi_pc@foxmail.com>
2019-06-06 01:23:06 +0800

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,5 +1,4 @@
 # Description:
-# Easy
 #
 # Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -9,6 +8,7 @@
 # Because nums[0] + nums[1] = 2 + 7 = 9,
 # return [0, 1].
 
+# Difficulty: Easy
 
 # Version 1 : 5512 ms	13.9 MB
 
